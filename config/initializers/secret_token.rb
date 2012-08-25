89fb47d7d1d64a9cd56933c5f3b4e8dba69f56eb
RogerGuTeng <answeriversongt@hotmail.com>
2012-08-25 01:53:21 -0400

    init commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Gutengweb::Application.config.secret_token = 'b3e1698815cc82c9e9ec09dba61b707e10b8c00b2b82de738086527753e7d0ec572502ea270de49c371a7e7aaaaa0f056e6fd6b8d16e857574be5cf3e3d12214'
